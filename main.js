e79f9ffeca7bb5642ecaeb83291c40d6c0a22ab2
Jibesh Kumar Panda <99865910+Jibesh04@users.noreply.github.com>
2022-05-29 22:47:09 +0530

    Added Basic Structure
    Added a reference site, to be modified for the landing page of the project

---
+++ main.js
@@ -0,0 +1,6 @@
+/* HAMBURGER MENU */
+const menuBtn = document.querySelector('.menu-btn')
+const navLinks = document.querySelector('.nav-links')
+menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('mobile-menu');
+})
